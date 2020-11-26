abc912e1d6690b9bf2e5b68b4836d1fa4c6cea2a
lilsherlock <potatogames15@gmail.com>
2020-11-26 13:36:32 +0000

    update

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,20 +1,26 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_insert_right - main
+ * @parent: tree
+ * @value: int
+ * Return: Right node
+ */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-        binary_tree_t *new_node;
-        if (parent == NULL)
-                return (NULL);
-        new_node = binary_tree_node(parent, value);
-        if (new_node == NULL)
-                return (NULL);
-        if (parent->right == NULL)
-                parent->right = new_node;
-        else
-        {
-                new_node->right = parent->right;
-                parent->right = new_node;
-                new_node->left->parent = new_node;
-        }
-        return (new_node);
+	binary_tree_t *new_node;
+
+	if (parent == NULL)
+		return (NULL);
+	new_node = binary_tree_node(parent, value);
+	if (new_node == NULL)
+		return (NULL);
+	if (parent->right == NULL)
+		parent->right = new_node;
+	else
+	{
+		new_node->right = parent->right;
+		parent->right = new_node;
+		new_node->left->parent = new_node;
+	}
+	return (new_node);
 }
