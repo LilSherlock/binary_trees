a625b678d24dc8d80c1eb4ff4cb6be376053ee8a
lilsherlock <potatogames15@gmail.com>
2020-11-26 13:30:58 +0000

    update

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -1,5 +1,10 @@
 #include "binary_trees.h"
-
+/**
+ * binary_tree_insert_left - main
+ * @parent: tree
+ * @value: int
+ * Return: left node
+ */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new;
